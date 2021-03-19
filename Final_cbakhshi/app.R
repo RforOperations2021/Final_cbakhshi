9aa84f24fec208e7012266462199605911cd4563
cbakhshi <cbakhshi@andrew.cmu.edu>
2021-03-19 19:10:00 -0400

    Added plots and data table in Plots page

--- Final_cbakhshi/app.R
+++ Final_cbakhshi/app.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(shinydashboard)
 library(leaflet)
+library(plotly)
 library(dplyr)
 library(leaflet.extras)
 library(tidyr)
@@ -20,7 +21,7 @@ earthquake.data = earthquake.data%>%
 # To avoid plotly issues -------------------------------------------------------
 pdf(NULL)
 
-# Application title ----------------------------------------------
+# Application title ------------------------------------------------------------
 header <- dashboardHeader(title = "Earthquakes (2020)", 
                           titleWidth = 250)
 
@@ -40,7 +41,7 @@ sidebar <- dashboardSidebar(width = 250,
 
 body <- dashboardBody(
     tabItems(
-        # Map page ----------------------------------------------
+        # Map page -------------------------------------------------------------
         tabItem("map-tab",
                 
                 h1("Earthquakes in the U.S. (2020)"),
@@ -53,16 +54,16 @@ body <- dashboardBody(
                 
                 br(),
                 
-                # Map ----------------------------------------------
+                # Map -----------------------------------------------------------
                 fluidRow(
                     box(
-                        # Select if heat map needed-----------
+                        # Select if heat map needed-----------------------------
                         checkboxInput("heatmap", "Show Heatmap", FALSE),
                         width = 200) 
                 ),
                 
                 fluidRow(    
-                    # Map Output
+                    # Map Output----------------------------------------------
                     leafletOutput("mymap")
                     
                     
@@ -73,9 +74,43 @@ body <- dashboardBody(
         # Map page ----------------------------------------------
         tabItem("plots-tab",
                 
-                #Value Boxes ------------------------------------------
+                #Value Boxes ---------------------------------------------------
                 fluidRow(valueBoxOutput("count"),
                          valueBoxOutput("avg.mag")),
+                
+                #plots tab box--------------------------------------------------
+                fluidRow(
+                    tabBox(title = "Plots",
+                           width = 10,
+                           tabPanel(strong("Total Earthquakes by Month"), plotlyOutput("plot1")),
+                           tabPanel(strong("Average Magnitude of Earthquakes"), plotlyOutput("plot2"))),
+                    
+                    box(
+                        # Select maximum mag for plots-----------------------
+                        sliderInput(inputId = "max.mag",
+                                    label = "Select Maximum Magnitude:",
+                                    min = 3, max = 6.5, step = 0.5,
+                                    value = 6.5),
+                        br(),
+                        
+                        # Show data table -----------------------------------
+                        checkboxInput(inputId = "show_data1",
+                                      label = "Show data table",
+                                      value = TRUE),
+                        
+                        width = 2)),
+                
+                fluidRow(
+                    #Download button -----------------------------------------------
+                    downloadButton(
+                        outputId = "downloadbutton",
+                        label = "Download",
+                        class = NULL,
+                        icon = shiny::icon("download")),
+                    
+                    # Box with data table 
+                    box(DT::dataTableOutput(outputId = "datatable1"), width = 12)
+                )
                 
         )
         
@@ -147,6 +182,55 @@ server <- function(input, output) {
                  value = avg, icon = icon("balance-scale-right"), color = "blue")
     })
     
+    # Create a subset of data for getting histogram upto max magnitude ------
+    max_mag <- reactive({
+        req(input$max.mag) # ensure availablity of value before proceeding
+        earthquake.data[earthquake.data$mag<= input$max.mag,]
+        
+    })
+    
+    # Create barplot object the plotOutput function is expecting --
+    output$plot1 <- renderPlotly({
+        ggplot(data = max_mag(), aes_string(x = "month"))+
+            geom_bar(fill= 'steelblue')+ theme_bw()+
+            labs(title = "Number of Earthquakes in the U.S. by month", 
+                 subtitle = "Choose the number max Magnitude",
+                 y = "Number of Earthquakes", 
+                 x = 'Months')
+    }
+    )
+    
+    # Create barplot object the plotOutput function is expecting --
+    output$plot2 <- renderPlotly({
+        ggplot(data = max_mag(), aes_string(x= "month", y ="mag"))+
+            geom_point(fill= 'steelblue')+ theme_bw()+
+            labs(title = "Magnitude of Earthquakes in the U.S. by month", 
+                 subtitle = "Choose the number max Magnitude",
+                 y = "Magnitude of Earthquakes", 
+                 x = 'Months')
+    }
+    )
+    
+    # Print data table if checked -------------------------------------
+    output$datatable1 <- DT::renderDataTable(
+        if(input$show_data1){
+            DT::datatable(data = max_mag()[, c("month", "day", "depth", 
+                                               "mag", "latitude",
+                                               "longitude","place")], 
+                          options = list(pageLength = 5), 
+                          rownames = FALSE)
+        }
+    )
+    
+    #Download data if checked 
+    output$downloadbutton <- downloadHandler(
+        filename = function(){
+            paste('file1.csv')
+        },
+        content = function(file) {
+            write.csv(max_mag(), file)
+        }
+    )
     
 }
 
